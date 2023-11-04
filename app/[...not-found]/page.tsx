6e20c13603a951205c2079e0372c0a691539c7e6
Abdalla Misbah <abdalla.misbah@rasid.com.sa>
2023-11-04 21:58:59 +0300

    feat: add general notfound page

---
+++ app/[...not-found]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const NotFound = (props: Props) => {
+  return (
+    <div className="bg-green-200">
+      <h1 className="text-green-500">Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
